d772d27756f2793a238756efbf7c6e08e089d0ea
Jeffrey Willis <jwillis@myocv.com>
2017-12-04 16:06:20 -0600

    Added builds

--- conf.lua
+++ conf.lua
@@ -2,6 +2,6 @@ function love.conf(t)
     t.window.width = 320
     t.window.height = 416
 
-    t.window.title = "Overtime Masters"
+    t.window.title = "Work Master"
     t.window.borderless = false
 end
